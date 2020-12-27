af389a280a821b3b0bf71b6a0dab4aac2285f81d
f-exuan21 <f.exuan21@gmail.com>
2020-12-27 14:12:24 +0900

    restrict 삭제

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,13 +6,13 @@
 /*   By: abang <abang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 17:06:34 by abang             #+#    #+#             */
-/*   Updated: 2020/12/22 19:27:46 by abang            ###   ########.fr       */
+/*   Updated: 2020/12/27 14:11:49 by abang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	unsigned int	i;
 	unsigned int	cnt;
