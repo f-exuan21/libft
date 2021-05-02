b673ff21e278be602256826bab3656af590a6bcc
f-exuan21 <f.exuan21@gmail.com>
2021-05-03 01:23:10 +0900

    norm

--- ft_atof.c
+++ ft_atof.c
@@ -6,11 +6,26 @@
 /*   By: abang <abang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/30 20:02:33 by abang             #+#    #+#             */
-/*   Updated: 2021/04/30 22:14:20 by abang            ###   ########.fr       */
+/*   Updated: 2021/05/03 01:22:22 by abang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int		check_minus_dot(char *s, int *minus_flag, float *flag)
+{
+	if (*s == '-')
+	{
+		*minus_flag = 1;
+		return (1);
+	}
+	else if (*s == '.')
+	{
+		*flag = 0.1;
+		return (1);
+	}
+	return (0);
+}
 
 float	ft_atof(char *s)
 {
@@ -21,17 +36,10 @@ float	ft_atof(char *s)
 	num = 0;
 	flag = 0;
 	minus_flag = 0;
-	while(*s)
+	while (*s)
 	{
-		if (*s == '-')
+		if (check_minus_dot(s, &minus_flag, &flag))
 		{
-			minus_flag = 1;
-			s++;
-			continue ;
-		}
-		if (*s == '.')
-		{
-			flag = 0.1;
 			s++;
 			continue ;
 		}
